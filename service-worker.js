a1fffa0e2a6840d8fa89420a8f812360911c7071
iosef182006 <182006+iosef182006@users.noreply.github.com>
2026-08-28 22:29:53 -0500

    Agrega tercera entrega a la PWA

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "aitana-pwa-v5";
+const CACHE_VERSION = "aitana-pwa-v6";
 const STATIC_CACHE = `${CACHE_VERSION}-static`;
 const NETWORK_CACHE = `${CACHE_VERSION}-network`;
 
@@ -14,7 +14,10 @@ const ESSENTIAL_FILES = [
   "/img/apple-touch-icon.png",
   "/img/icon-192.png",
   "/img/icon-512.png",
-  "/img/icon-maskable-512.png"
+  "/img/icon-maskable-512.png",
+  "/img/entregas-01-rostro-difuminado.png",
+  "/img/entregas-02-rostro-difuminado.png",
+  "/img/entregas-03-rostro-difuminado.png"
 ];
 
 const ICON_ASSETS = new Set([

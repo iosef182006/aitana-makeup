31bcabca5372de55961ec70e930ab642a817277a
iosef182006 <182006+iosef182006@users.noreply.github.com>
2026-09-22 18:33:58 -0500

    Blur faces in latest delivery photos

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "aitana-pwa-v21";
+const CACHE_VERSION = "aitana-pwa-v22";
 const STATIC_CACHE = `${CACHE_VERSION}-static`;
 const NETWORK_CACHE = `${CACHE_VERSION}-network`;
 

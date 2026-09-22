1aa167a1baf623d2cfad22cb5cbb812063f48237
iosef182006 <182006+iosef182006@users.noreply.github.com>
2026-09-22 18:29:08 -0500

    Add four new customer delivery photos

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "aitana-pwa-v20";
+const CACHE_VERSION = "aitana-pwa-v21";
 const STATIC_CACHE = `${CACHE_VERSION}-static`;
 const NETWORK_CACHE = `${CACHE_VERSION}-network`;
 
